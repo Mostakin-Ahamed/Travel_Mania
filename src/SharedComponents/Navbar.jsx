543c10f068b788f3490323ea3cf2a2e77b07c8fb
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2023-11-27 23:16:15 +0600

    navbar work done

--- src/SharedComponents/Navbar.jsx
+++ src/SharedComponents/Navbar.jsx
@@ -7,6 +7,7 @@ import { AuthContext } from "../Provider/AuthProvider";
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext)
+    console.log(user);
     const handleSignOut = () => {
         logOut()
             .then()
@@ -15,7 +16,10 @@ const Navbar = () => {
 
     const navLinks = <>
         <li><NavLink className="mx-3" to='/'>Home</NavLink></li>
-        <li><NavLink className="mx-3" to='/services'>Services</NavLink></li>
+        <li><NavLink className="mx-3" to='/community'>Community</NavLink></li>
+        <li><NavLink className="mx-3" to='/blogs'>Blogs</NavLink></li>
+        <li><NavLink className="mx-3" to='/aboutUs'>About Us</NavLink></li>
+        <li><NavLink className="mx-3" to='/contactUs'>Contact Us</NavLink></li>
     </>
 
     return (
@@ -32,21 +36,13 @@ const Navbar = () => {
                                     navLinks
                                 }
                                 {
-                                    user ?
-                                        <li className="">
-                                            <a>Dashboard</a>
-                                            <ul className="p-2">
-                                                <li><Link to='/myServices'><a>My Services</a></Link></li>
-                                                <li><Link to='/addService'><a>Add Services</a></Link></li>
-                                                <li><Link to='/mySchedules'><a>My Schedules</a></Link></li>
-                                            </ul>
-                                        </li> : <li><NavLink className="mx-3" to='/login'>Login</NavLink></li>
+                                    user ?<></> : <li><NavLink className="mx-3" to='/login'>Login</NavLink></li>
                                 }
 
                             </ul>
                         </div>
-                        <div className="lg:flex md:flex"><img className="h-[35px] ml-3" src="/logo.svg" alt="" />
-                        <Link to="/"><a className="btn btn-ghost normal-case text-xl">ToolShare</a></Link></div>
+                        <div className="lg:flex md:flex"><img className="h-[35px] ml-3 pl-r" src="/icon.svg" alt="" />
+                        <Link to="/"><a className="btn btn-ghost normal-case text-xl">Travel Mania</a></Link></div>
                     </div>
                     <div className="navbar-center hidden lg:flex">
                         <ul className="menu menu-horizontal px-1">
@@ -54,18 +50,8 @@ const Navbar = () => {
                                 navLinks
                             }
                             {
-                                user ?
-                                    <li tabIndex={0}>
-                                        <details>
-                                            <summary>Dashboard</summary>
-                                            <ul className="p-2">
-                                                <li><Link to='/myServices'><a>My Services</a></Link></li>
-                                                <li><Link to='/addService'><a>Add Services</a></Link></li>
-                                                <li><Link to='/mySchedules'><a>My Schedules</a></Link></li>
-
-                                            </ul>
-                                        </details>
-                                    </li> : <li><NavLink className="mx-3" to='/login'>Login</NavLink></li>
+                                user ?<></>:
+                                    <li><NavLink className="mx-3" to='/login'>Login</NavLink></li>
                             }
 
                         </ul>
@@ -76,15 +62,23 @@ const Navbar = () => {
                             user && <h3 className="text-bold font-2xl mr-5">{user.displayName}</h3>
                         }
                         {
-                            user && <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                                <div className="w-10 rounded-full">
-                                    <img src={user.photoURL} />
-                                </div>
-                            </label>
+                            user && <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                              <div className="w-10 rounded-full">
+                                <img src={user.photoURL} />
+                              </div>
+                            </div>
+                            <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                              <li>
+                                <a className="justify-between">
+                                  Dashboard
+                                </a>
+                              </li>
+                              <li><button onClick={handleSignOut}> Log Out</button></li>
+                            </ul>
+                          </div>
                         }
-                        {
-                            user && <button onClick={handleSignOut} className="btn bg-red-300 ml"> Log Out</button>
-                        }
+                        
 
                     </div>
                 </div>
