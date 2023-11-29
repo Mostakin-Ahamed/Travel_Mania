9dba8af88dcc4f831975a7f7b084fa54434264d9
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2023-11-29 11:14:27 +0600

    user rating part done

--- src/Pages/Home/Components/TouristStory/Story.jsx
+++ src/Pages/Home/Components/TouristStory/Story.jsx
@@ -1,9 +1,44 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
+import './styles.css';
+import { useEffect, useState } from 'react';
 const Story = () => {
+
+    const [reviews, setReviews]= useState([])
+    useEffect(()=>{
+        fetch('/Rating.json')
+        .then(res => res.json())
+        .then(data =>setReviews(data))
+    })
+
+
     return (
         <div>
-            <h2>This is tourist story section</h2>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                
+                {
+                    reviews.map(review => <SwiperSlide key={review.user_id} >
+                        <div>
+                            <p className='mb-5'>{review.comment}</p>
+                            <h2 className='text-3xl text-orange-400 mb-5'>By : {review.username}</h2>
+                            <h2 className='text-2xl'>Rating: {review.rating}/5</h2>
+                        </div></SwiperSlide>)
+                }
+            </Swiper>
         </div>
     );
 };
