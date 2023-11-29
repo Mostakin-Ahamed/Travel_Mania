211feff28e6061f1396d405b675af42235664e48
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2023-11-29 09:31:27 +0600

    overview tab done

--- src/Pages/Home/Components/Tabs/TourismAndTravel.jsx
+++ src/Pages/Home/Components/Tabs/TourismAndTravel.jsx
@@ -2,8 +2,19 @@ import { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './styles.css';
+import { Pagination } from 'swiper/modules';
+
 const TourismAndTravel = () => {
-    const [tabIndex, setTabIndex]= useState(0)
+    const [tabIndex, setTabIndex] = useState(0)
     return (
         <div className=' mx-auto lg:w-10/12  justify-center'>
             <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
@@ -12,9 +23,24 @@ const TourismAndTravel = () => {
                     <Tab>Our Package</Tab>
                     <Tab>Meet Our Tour Guides</Tab>
                 </TabList>
+                <TabPanel>
+                    <Swiper pagination={true} modules={[Pagination]} className="mySwiper h-72">
+                        <SwiperSlide><img src="../../../../../public/slide1.jpg" alt="" />
+                        </SwiperSlide>
+                        <SwiperSlide><img src="../../../../../public/slide2.jpg" alt="" />
+                        </SwiperSlide>
+                        <SwiperSlide><img src="../../../../../public/slide3.jpg" alt="" />
+                        </SwiperSlide>
+                        <SwiperSlide><img src="../../../../../public/slide4.jpg" alt="" />
+                        </SwiperSlide>
+                        <SwiperSlide><img src="../../../../../public/slide5.jpg" alt="" />
+                        </SwiperSlide>
+                        <SwiperSlide><img src="../../../../../public/slide6.jpg" alt="" />
+                        </SwiperSlide>
+                    </Swiper>
+                </TabPanel>
                 <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
-                <TabPanel></TabPanel>   
             </Tabs>
             <hr className='border-dashed border-gray-400' />
         </div>
