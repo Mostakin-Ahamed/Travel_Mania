72fa6e38016d06129d8f78fa52f1ccdcea66f37b
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2023-11-30 10:09:48 +0600

    wishlist part done and tour booking part done

--- src/Components/TourCard.jsx
+++ src/Components/TourCard.jsx
@@ -1,19 +1,70 @@
 import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa";
+
+import { useContext, useRef } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import Swal from "sweetalert2";
+
+
 
 // eslint-disable-next-line react/prop-types
-const TourCard = ({item}) => {
+const TourCard = ({ item }) => {
+    const { user } = useContext(AuthContext)
+    const formRef = useRef()
 
     const {
         // eslint-disable-next-line react/prop-types
-        _id,service_name,description,price,duration_hours,tour_type, image} = item;
+        _id, service_name, description, price, duration_hours, tour_type, image } = item;
+
+
+    const addToWishlist = e => {
+        e.preventDefault();
+        // const wishlist = { userEmail, item }
+        
+        const email = user.email;
+        const tourId = _id;
+        const tourImage = image;
+        const tourDescription = description;
+        const tourPrice = price;
+        const tourDuration = duration_hours;
+        
+        const wishList = { email, tourId, tourImage, tourDescription, tourPrice, tourDuration};
+        console.log(wishList);
+
+        fetch('http://localhost:5000/wishList', {
+
+                method: 'POST',
+                headers:{
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(wishList)
+
+            })
+            .then(res => res.json())
+            .then(data =>{
+                if(data.insertedId){
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Tour added to WishList!',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                    formRef.current?.reset();
+                }
+            })
+
+
+    }
 
     return (
         <div className="max-h-[650px] pl-32 mb-10">
             <div className="card w-96 h-full bg-base-100 shadow-xl">
-                <figure className="px-10 pt-10">
-                    <img src={image} alt="Shoes" className="rounded-xl" />
+                <figure className=" px-10 pt-10">
+                    <img src={image} alt="Shoes" className="rounded-xl object-fill h-[200px]" />
+
                 </figure>
                 <div className="card-body items-center text-center">
+                    <h2><button onClick={addToWishlist} className="btn-lg text-red-500"><FaHeart /></button></h2>
                     <h2 className="card-title">Service Name: {service_name}</h2>
                     <h2 className="card-title">Service Type: {tour_type}</h2>
                     <h2 className="card-title">Price: ${price}</h2>
